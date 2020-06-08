3f2bd0fa5dda20d5231804009c66a75fe8aeb41a
Wilker Lopes Cruz <wilkerlopes257@gmail.com>
2020-06-07 21:44:59 -0400

    newIcones e instaciamento

---
+++ js/geral.js
@@ -0,0 +1,13 @@
+$(function() {
+
+
+    $("#iconSearch").keyup(function() {
+        var iconsearch = $(this).val();
+
+        $("#iconsList li").show();
+        $("#iconsList li").each(function() {
+            if ($(this).text().toUpperCase().indexOf(iconsearch.toUpperCase()) < 0)
+                $(this).hide();
+        });
+    });
+});
