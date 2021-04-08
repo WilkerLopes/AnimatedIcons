7d7f0ecd41d00fdffd9a89cd7b64141e0e395010
Wilker Lopes <wilkerlopes257@gmail.com>
2021-04-08 09:08:04 -0400

    fixing all 'brakes'

--- js/animatedIcons.js
+++ js/animatedIcons.js
@@ -12,22 +12,16 @@ function createIcons(icon) {
             return engrenagem;
         case "perfil":
             return perfil;
-            break;
         case "artigo":
             return artigo;
-            break;
         case "menuVolta2":
             return menuVolta2;
-            break;
         case "menuVolta3":
             return menuVolta3;
-            break;
         case "menuFechar":
             return menuFechar;
-            break;
         case "playPausar":
             return playPausar;
-            break;
     }
 }
 
@@ -35,18 +29,17 @@ function invertColor(hex) {
     if (hex.indexOf('#') === 0) {
         hex = hex.slice(1);
     }
-    // convert 3-digit hex to 6-digits.
+    
     if (hex.length === 3) {
         hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
     }
     if (hex.length !== 6) {
         throw new Error('Invalid HEX color.');
     }
-    // invert color components
+    
     var r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16),
         g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16),
         b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
-    // pad each with zeros and return
     return '#' + padZero(r) + padZero(g) + padZero(b);
 }
 
